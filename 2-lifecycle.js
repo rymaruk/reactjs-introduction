942ace464099c5448073804b51df54d7053dc344
Andrew Rymaruk <andrewrymaruk@gmail.com>
2018-09-03 17:44:41 +0300

    Lifecycle Component

---
+++ 2-lifecycle.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+            age: 21,
+        };
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.setState({date: new Date()}), 1000);
+    }
+
+    componentWillUpdate(){
+        // console.log('Now will be TICK!');
+    }
+
+    componentDidUpdate(){
+        // console.log('Tick!');
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // console.log(prevProps, prevState);
+    }
+
+    buttonHandler(event){
+        this.setState({age: event.target.value});
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>My name is, {this.props.name}! I'm {this.state.age} years old!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <input type={'number'} onChange={this.buttonHandler.bind(this)} />
+            </div>
+        );
+    }
+
+}
+
+export default App;
