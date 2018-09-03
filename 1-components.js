e9e6f33ad64dbfb968472b8f9a8c2a0b3a54d0a3
Andrew Rymaruk <andrewrymaruk@gmail.com>
2018-09-03 14:36:24 +0300

    Update 1-components.js

--- 1-components.js
+++ 1-components.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 
-// function App(props) {
-//     // props = {name: 'User'}
-//     return <h1>Hello, {props.name}</h1>;
-// }
+function App(props) {
+    // props = {name: 'User'}
+    return <h1>Hello, {props.name}</h1>;
+}
 
 // const App = props => <h1>Hello, {props.name}</h1>;
 
-class App extends Component {
-    constructor() {
-        super(); // super(props);
-        this.state = { age: 21 };
-    }
-    // this.props = {name: 'User'}
-    render() {
-        return <h1>Hello, {this.props.name}!</h1>;
-    }
-}
+// class App extends Component {
+//     constructor() {
+//         super(); // super(props);
+//         this.state = { age: 21 };
+//     }
+//     // this.props = {name: 'User'}
+//     render() {
+//         return <h1>My name is, {this.props.name}!. I'm {this.state.age} years old!</h1>;
+//     }
+// }
 
 export default App;
