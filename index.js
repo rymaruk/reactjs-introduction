6492e21e093240dcb82ab444379c7322f775a8ef
Andrew Rymaruk <andrewrymaruk@gmail.com>
2018-09-03 14:35:49 +0300

    Create index.js
    React. RaectDOM. ReactDOM.render()

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+
+ReactDOM.render(<h1>Hello world!</h1>, document.getElementById('root'));
+
+// ReactDOM.render(<App name={'User name'} />, document.getElementById('root'));
