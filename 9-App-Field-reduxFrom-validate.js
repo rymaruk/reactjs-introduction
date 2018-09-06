73f8508f82c7ab909d81331a759d2d205be68a5b
Andrew Rymaruk <andrewrymaruk@gmail.com>
2018-09-06 17:41:51 +0300

    Update 9-App-Field-reduxFrom-validate.js

--- 9-App-Field-reduxFrom-validate.js
+++ 9-App-Field-reduxFrom-validate.js
@@ -2,25 +2,7 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 import {validate} from './validate';
-
-const InputField = ( { input, label, type, meta: { touched, error, warning },} ) => {
-    return (
-        <div>
-            <div className="form-group">
-                <label>
-                    <h5 style={touched &&  error ? {'color': 'red'} : null}>
-                        {label}
-                        {touched &&  error &&
-                        <div className="ml-2 badge badge-secondary" style={{'background': 'red'}}>
-                            {error}
-                        </div>}
-                    </h5>
-                </label>
-                <input className={error && touched ? 'is-invalid form-control' : 'form-control'} {...input} type={type} />
-            </div>
-        </div>
-    );
-};
+import {InputField} from './InputField';
 
 
 let SomeForm = props => {
