d0be368850a06ad2712460e504dbf5974bb56f74
Andrew Rymaruk <andrewrymaruk@gmail.com>
2018-09-06 17:40:04 +0300

    Create 10-SelectField.js

---
+++ 10-SelectField.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {Field} from "redux-form";
+
+export const SelectField = ( { input, label, children, meta: { touched, error, warning },} ) => {
+    return (
+        <div>
+            <div className="form-group">
+                <label>
+                    <h5 style={touched &&  error ? {'color': 'red'} : null}>
+                        {label}
+                        {touched &&  error &&
+                        <div className="ml-2 badge badge-secondary" style={{'background': 'red'}}>
+                            {error}
+                        </div>}
+                    </h5>
+                </label>
+                <select className={error && touched ? 'is-invalid form-control' : 'form-control'} {...input}>
+                    {children}
+                </select>
+            </div>
+        </div>
+    );
+};
